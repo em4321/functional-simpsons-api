51d7b5d00ecdbd96141e0639af5969ecbb60280c
Your Name <you@example.com>
2024-02-23 01:06:49 +0000

    updated buttons and styling

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-const Image = ({ src, alt }) => {
-  //Defensive check
-  return typeof src !== "string" ||
-    typeof alt !== "string" ||
-    src.length === 0 ||
-    alt.length === 0 ? (
-    <p>Image is not available</p>
-  ) : (
-    <img src={src} alt={alt} />
-  );
-};
+// import React from "react";
+// const Image = ({ src, alt }) => {
+//   //Defensive check
+//   return typeof src !== "string" ||
+//     typeof alt !== "string" ||
+//     src.length === 0 ||
+//     alt.length === 0 ? (
+//     <p>Image is not available</p>
+//   ) : (
+//     <img src={src} alt={alt} />
+//   );
+// };
 
-export default Image;
+// export default Image;
